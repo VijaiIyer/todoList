5f9332e9976ee88a5f64c12c5cd507838b73ab5b
Vijai Iyer <vijaiyer2000@gmail.com>
2021-06-22 18:34:57 +0530

    logout issue

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react'
 import { firebase } from './src/firebase/config'
 import { Button ,View} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
+
+import { NavigationContainer,useNavigation } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
 import {decode, encode} from 'base-64'
@@ -17,12 +18,15 @@ export default function App() {
   const [user, setUser] = useState(null)
 
 
-  signOutUser = async (navigation) => {
+  signOutUser = async () => {
     try {
         await firebase.auth().signOut();
-        navigation.navigate('LoginScreen');
+        return true;
+        // const navigation= useNavigation();
+        // navigation.navigate('LoginScreen');
     } catch (e) {
         console.log(e);
+        return false;
     }
 }
 
@@ -57,16 +61,19 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         { user ? (
-          <Stack.Screen name="HomeScreen" options={({ navigation, route }) => ({headerRight: ({}) => (
+          <Stack.Screen name="HomeScreen" options={
+            ({ navigation }) => ({headerRight: (navigation) => (
             <View style={{marginRight:20}}>
             <Button
-              onPress={() => {signOutUser(navigation)}}
+              onPress={() => {var res=signOutUser();
+              if(res)
+              navigation.navigate('LoginScreen');}}
               title="Logout"
               color="blue"
             />
             </View>
-          ),})
-            }>
+          )
+  })}>
             {props => <HomeScreen {...props} extraData={user} />}
           </Stack.Screen>
         ) : (
